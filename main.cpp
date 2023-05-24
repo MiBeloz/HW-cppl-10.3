fc7b04750d3d66bcc92a6ed3821d6a77a9d4b600
MiBeloz <miha_bmv_@mail.ru>
2023-05-24 16:18:41 +0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <memory>
+
+#include "unique_ptr.h"
+
+template<typename T>
+void print_ptr(T& ptr, T& ptr2);
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	std::cout << "\tstd::unique_ptr своими руками\n" << std::endl;
+
+	std::cout << "std::unique_ptr:" << std::endl;
+	std::unique_ptr<int> orig_unique_ptr(new int(7));
+	std::unique_ptr<int> orig_unique_ptr2(new int(5));
+	print_ptr(orig_unique_ptr, orig_unique_ptr2);
+	std::cout << std::endl << std::endl;
+
+	std::cout << "unique_ptr(своя реализация):" << std::endl;
+	unique_ptr<int> my_unique_ptr(new int(7));
+	unique_ptr<int> my_unique_ptr2(new int(5));
+	print_ptr(my_unique_ptr, my_unique_ptr2);
+
+	system("pause > nul");
+	return 0;
+}
+
+template<typename T>
+void print_ptr(T& ptr, T& ptr2) {
+	std::cout << "\tАдрес: " << ptr << std::endl;
+
+	std::cout << "\tЗначение: " << *ptr << std::endl << std::endl;
+
+	std::cout << "\tМетод 'get()': " << ptr.get() << " : " << *ptr << std::endl;
+	std::cout << "\tМетод 'get()' у unique_prt2: " << ptr2.get() << " : " << *ptr2 << std::endl << std::endl;
+	
+	std::cout << "\tМетод 'swap()': " << std::endl;
+	ptr.swap(ptr2);
+	std::cout << "\tМетод 'get()': " << ptr.get() << " : " << *ptr << std::endl;
+	std::cout << "\tМетод 'get()' у unique_prt2: " << ptr2.get() << " : " << *ptr2 << std::endl << std::endl;
+
+	std::cout << "\tАдрес: " << ptr << std::endl;
+
+	std::cout << "\tМетод 'operator bool()': " << std::boolalpha << ptr.operator bool() << std::endl;
+	std::cout << "\tМетод 'reset()': " << std::endl;
+	ptr.reset();
+	std::cout << "\tМетод 'operator bool()': " << std::boolalpha << ptr.operator bool() << std::endl;
+	std::cout << "\tАдрес: " << ptr << std::endl;
+}
---
+++ unique_ptr.h
@@ -0,0 +1,77 @@
+﻿#pragma once
+
+
+template<class T>
+class unique_ptr {
+public:
+	explicit unique_ptr(T* const ptr = nullptr) : m_ptr(ptr) {};
+	
+	~unique_ptr();
+	
+	T& operator*() const;
+	T* operator->() const;
+	T* get() const;
+	T* release();
+	void reset(T* const new_ptr = nullptr);
+	void swap(unique_ptr& right);
+	explicit operator bool() const;
+
+	unique_ptr(const unique_ptr&) = delete;
+	unique_ptr& operator=(const unique_ptr&) = delete;
+
+	friend std::ostream& operator<<(std::ostream& os, const unique_ptr<T>& obj) {
+		return os << &*obj;
+	}
+
+private:
+	T* m_ptr;
+};
+
+template<class T>
+inline unique_ptr<T>::~unique_ptr() {
+	delete m_ptr;
+	m_ptr = nullptr;
+}
+
+template<class T>
+T& unique_ptr<T>::operator*() const {
+	return *m_ptr;
+}
+
+template<class T>
+T* unique_ptr<T>::operator->() const {
+	return m_ptr;
+}
+
+template<class T>
+T* unique_ptr<T>::get() const {
+	return m_ptr;
+}
+
+template<class T>
+T* unique_ptr<T>::release() {
+	T* other_ptr = m_ptr;
+	m_ptr = nullptr;
+	return other_ptr;
+}
+
+template<class T>
+void unique_ptr<T>::reset(T* const new_ptr) {
+	if (m_ptr) {
+		delete m_ptr;
+		m_ptr = nullptr;
+	}
+	if (new_ptr) {
+		m_ptr = new_ptr;
+	}
+}
+
+template<class T>
+void unique_ptr<T>::swap(unique_ptr& right) {
+	std::swap(m_ptr, right.m_ptr);
+}
+
+template<class T>
+unique_ptr<T>::operator bool() const {
+	return static_cast<bool>(m_ptr);
+}
